c8d9f2aa38153cb526f3614907edf385d6b35f53
Naoko <69491257+N40K0@users.noreply.github.com>
2022-01-15 17:21:20 +0700

    Updating Website

--- docs/Javascript/index.js
+++ docs/Javascript/index.js
@@ -1,12 +1,36 @@
-let dropZone = document.querySelector(".drop-zone")
+const dropZone = document.querySelector(".drop-zone")
+const fileInput = document.querySelector(".input-file")
+const model = document.AIModel
 
-
-dropZone.addEventListener("dragover", (e) => {
-    e.preventDefault()
+dropZone.addEventListener("dragover", (event) => {
+    event.preventDefault()
 })
 
-dropZone.addEventListener("drop", (e) => {
-    e.stopPropagation()
-    e.preventDefault()
-    console.log(e.dataTransfer)
+dropZone.addEventListener("drop", (event) => {
+    event.stopPropagation()
+    event.preventDefault()
+
+    event.dataTransfer.files
+});
+
+dropZone.addEventListener("click", (event) => {
+    fileInput.click()
+    console.log(fileInput.files)
 })
+
+function predict(files) {
+    newLine("")
+    newLine("Predicting . . .")
+
+    files.forEach(file => {
+        let reader = new FileReader()
+        let image = new Image(256, 256)
+
+        reader.readAsDataURL(file)
+
+        reader.onload = () => {
+            image.src = reader.result
+
+        }
+    })
+}
--- docs/Javascript/modelLoader.js
+++ docs/Javascript/modelLoader.js
@@ -1,6 +1,21 @@
-const model = tf.loadGraphModel("./Model/model.json")
-let modelLoaded
+function newLine(text, breakLine = true) {
+    const predictionLogs = document.querySelector(".prediction-logs")
+    const line = document.createElement("a")
+    const br = document.createElement("br")
 
-model.then((model)=>{
-    modelLoaded = true
-})
+    if(breakLine) predictionLogs.appendChild(br) 
+    line.textContent = text + " "
+    predictionLogs.appendChild(line)
+}
+
+(function () {
+    const loadModel = tf.loadGraphModel("./Model/model.json")
+
+    newLine("Loading . . .")
+    loadModel.then((model) =>    {
+        newLine("Done!", false)
+        document.AIModel = model
+        document.AIModel.loaded = true
+    })
+
+})()
